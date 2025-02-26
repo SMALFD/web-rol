9841ed9874008c7f2e5abf2a1393fdd65e698642
SMALFD <santiagomonreal03@gmail.com>
2025-02-27 00:22:37 +0100

    Erased unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import ThemeWrapper from "./components/Themewrapper/Themewrapper";
 import "./styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
