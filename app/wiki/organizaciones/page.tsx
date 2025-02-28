39bb30b9a5e18f8950aee9d2c18f9f6c607c2806
SMALFD <santiagomonreal03@gmail.com>
2025-02-28 11:51:19 +0100

    Reorganized pages to help for easier maintenance

--- app/wiki/organizaciones/page.tsx
+++ app/wiki/organizaciones/page.tsx
@@ -8,49 +8,49 @@ export default function Organizaciones() {
                 <h1 id="organizaciones">Organizaciones</h1>
                 <ul>
                     <li>
-                        <Link href="/wiki/imperio">Imperio</Link>
+                        <Link href="/wiki/organizaciones/imperio">Imperio</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/torre-magica-roja">Torre Mágica Roja</Link>
+                        <Link href="/wiki/organizaciones/torre-magica-roja">Torre Mágica Roja</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/torre-magica-azul">Torre Mágica Azul</Link>
+                        <Link href="/wiki/organizaciones/torre-magica-azul">Torre Mágica Azul</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/gremio-de-asesinos">Gremio de Asesinos</Link>
+                        <Link href="/wiki/organizaciones/gremio-de-asesinos">Gremio de Asesinos</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/gremio-de-aventureros">Gremio de Aventureros</Link>
+                        <Link href="/wiki/organizaciones/gremio-de-aventureros">Gremio de Aventureros</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/culto-bajo-ciudad">Culto bajo la ciudad</Link>
+                        <Link href="/wiki/organizaciones/culto-bajo-ciudad">Culto bajo la ciudad</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/grupo-de-bandidos">Grupo de bandidos</Link>
+                        <Link href="/wiki/organizaciones/grupo-de-bandidos">Grupo de bandidos</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/iglesia">Iglesia</Link>
+                        <Link href="/wiki/organizaciones/iglesia">Iglesia</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/guardia-de-roble">Guardia de Roble</Link>
+                        <Link href="/wiki/organizaciones/guardia-de-roble">Guardia de Roble</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/islas-de-azir">Islas de Azir</Link>
+                        <Link href="/wiki/organizaciones/islas-de-azir">Islas de Azir</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/comuna-independiente-del-bosque">Comuna independiente del Bosque de</Link>
+                        <Link href="/wiki/organizaciones/comuna-independiente-del-bosque">Comuna independiente del Bosque de</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/el-dragon-verde">El Dragón Verde</Link>
+                        <Link href="/wiki/organizaciones/el-dragon-verde">El Dragón Verde</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/la-gaceta-imperial">La Gaceta Imperial</Link>
+                        <Link href="/wiki/organizaciones/la-gaceta-imperial">La Gaceta Imperial</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/los-heroes-imperiales">Los Héroes Imperiales</Link>
+                        <Link href="/wiki/organizaciones/los-heroes-imperiales">Los Héroes Imperiales</Link>
                     </li>
                     <li>
-                        <Link href="/wiki/las-tribus-del-norte">Las tribus del Norte</Link>
+                        <Link href="/wiki/organizaciones/las-tribus-del-norte">Las tribus del Norte</Link>
                     </li>
                 </ul>
             </div>
