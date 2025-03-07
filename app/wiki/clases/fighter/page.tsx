1a0e6460d25b88f3f8449b6f76711016655cb359
SMALFD <santiagomonreal03@gmail.com>
2025-03-07 22:55:45 +0100

    added fighter

--- app/wiki/clases/fighter/page.tsx
+++ app/wiki/clases/fighter/page.tsx
@@ -6,7 +6,14 @@ export default function Fighter() {
             <div className="content">
                 <h1 id="fighter">Guerrero</h1>
                 <p>
-                    
+                    Los fighters son el saco de los guerreros, los soldados, los duelistas, los habilidosos de la espada, 
+                    arco, maza o escudo improvisado con una puerta. Son todos aquellos jóvenes que han entrenado duro con 
+                    toda clase de armas para lograr su estatua en el coliseo, aquellos que saben lo que es el esfuerzo 
+                    de trabajar sus habilidades hasta obtener la perfección y ser maestros del combate. Muchos son lo que tiene talento 
+                    y muchos más son lo que a falta de don natural y decidieron no rendirse y tomar las riendas de su destino
+                    para demostrarle a mundo y a si mismos de lo que son capaces. Son gente de todas las tierras y entornos,
+                    no hay clases ni estatus, no hay divisiones salvo lo que un guerrero PUEDE hacer y que NO. Y es ahí
+                    donde su grandeza o su empeño brillan con luz propia.
                 </p>
             </div>
         </main>
